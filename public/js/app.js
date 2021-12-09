b22b564df3c2d50088a0b94a93ce4439e34aff39
Rajinikanth reddy varala <rajinikanthreddyvarala@gmail.com>
2021-12-09 11:36:29 +0530

    setup an App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,35 +14,35 @@ console.log("client side javacsript file is loaded")
 
 })*/
 
-const weatherform=document.querySelector('form')
-const search=document.querySelector('input')
-const messageOne=document.querySelector('#message-1')
-const messageTwo=document.querySelector('#message-2')
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-weatherform.addEventListener('submit',(event)=>{
+weatherform.addEventListener('submit', (event) => {
     event.preventDefault();
-    const location=search.value;
-    if(location){
-        messageOne.textContent="Loadind data Please wait"
-        messageTwo.textContent=''
+    const location = search.value;
+    if (location) {
+        messageOne.textContent = "Loadind data Please wait"
+        messageTwo.textContent = ''
 
-        fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){ 
-            messageOne.textContent=data.error;
-        }
-        else{
-             messageOne.textContent=data.location
-             messageTwo.textContent=data.forecast
-           
+        fetch(`/weather?address=${location}`).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error;
+                }
+                else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
 
-        }
-    })
 
-})
+                }
+            })
+
+        })
 
     }
-    else{
+    else {
         alert('Please enter address to search')
     }
 })
